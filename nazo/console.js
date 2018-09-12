f7edb1cd0c13a406b848c7e37174e11374aed4fa
qq1010903229 <37618983+qq1010903229@users.noreply.github.com>
2018-09-12 18:03:55 +0800

    Create console.js

---
+++ nazo/console.js
@@ -0,0 +1,18 @@
+function isMobile(){
+var ua = navigator.userAgent;
+var ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
+isIphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
+isAndroid = ua.match(/(Android)\s+([\d.]+)/),
+isMobile = isIphone || isAndroid;
+if(isMobile) {
+return true;
+}else {
+return false;
+}
+}
+
+if(isMobile()){
+if(document.location.pathname==
+"/mynazo/console.html")document.location.href="blank.html";
+else setTimeout("document.location.href=\"blank.html\";",10000);
+}
