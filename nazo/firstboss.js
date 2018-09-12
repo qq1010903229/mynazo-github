c1d865633665192fb8f8eaee4f497082080571e0
qq1010903229 <37618983+qq1010903229@users.noreply.github.com>
2018-09-12 18:17:42 +0800

    Add JS For Stage 10
    Add JS For Stage 10

---
+++ nazo/firstboss.js
@@ -0,0 +1,7 @@
+var c=1000000;
+function attack(){
+c--;
+if(c<=0)document.location.href="dimension/dimension.html";
+document.getElementById("c").innerHTML=c;
+}
+document.getElementById("c").innerHTML=c;
